02b4b8cf47465e245d9ccb6e927020e1f5e44ed5
qwilton-god <antoshakopakov@gmail.com>
2025-06-14 17:20:58 +0300

    try9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.messenger_by_qwilton"
-    compileSdk = 31
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -27,8 +27,8 @@ android {
         applicationId = "com.example.messenger_by_qwilton"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         
--- android/app/src/main/kotlin/com/example/messenger_by_qwilton/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/messenger_by_qwilton/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.messenger_by_qwilton
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
